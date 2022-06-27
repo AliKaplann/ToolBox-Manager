60e4786322c8fd7378835bd6126bad572226f0f6
AliKaplann <103608939+AliKaplann@users.noreply.github.com>
2022-06-27 14:33:28 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,7 @@ namespace ToolBox_Manager
 {
     public partial class Form1 : Form
     {
-        WebClient Kaplan;
+        WebClient Tiger;
 
         public Form1()
         {
@@ -14,51 +14,57 @@ namespace ToolBox_Manager
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://download.scdn.co/SpotifySetup.exe", @"C:\windows\temp\spotf.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://download.scdn.co/SpotifySetup.exe", @"C:\windows\temp\spotf.exe");
             System.Diagnostics.Process.Start(@"C:\windows\temp\spotf.exe");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://www.7-zip.org/a/7z2200-x64.exe", @"C:\windows\temp\zip.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://www.7-zip.org/a/7z2200-x64.exe", @"C:\windows\temp\zip.exe");
             System.Diagnostics.Process.Start(@"c:windows\temp\zip.exe");
         }
 
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9005/DiscordSetup.exe", @"c:\windows\temp\discord.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9005/DiscordSetup.exe", @"c:\windows\temp\discord.exe");
             System.Diagnostics.Process.Start(@"C:\windows\temp\discord.exe");
         }
         private void button4_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://cdn-fastly.obsproject.com/downloads/OBS-Studio-27.2.4-Full-Installer-x64.exe", @"C:\windows\temp\obs.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://cdn-fastly.obsproject.com/downloads/OBS-Studio-27.2.4-Full-Installer-x64.exe", @"C:\windows\temp\obs.exe");
             System.Diagnostics.Process.Start(@"C:\windows\temp\obs.exe");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Uygulama yükleniyor...");
-            Kaplan.DownloadFile("https://github.com/ShareX/ShareX/releases/download/v13.7.0/ShareX-13.7.0-setup.exe", @"c:\windows\temp\share.exe");
+            Tiger.DownloadFile("https://github.com/ShareX/ShareX/releases/download/v13.7.0/ShareX-13.7.0-setup.exe", @"c:\windows\temp\share.exe");
             System.Diagnostics.Process.Start(@"c:\windows\temp\share.exe");
-            MessageBox.Show("Yüklendi kuruluyor...");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://dl.bandicam.com/bdcamsetup.exe", @"C:\windows\temp\bdcam.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://dl.bandicam.com/bdcamsetup.exe", @"C:\windows\temp\bdcam.exe");
             System.Diagnostics.Process.Start(@"C:\windows\temp\bdcam.exe");
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe", @"c:\windows\temp\steam.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe", @"c:\windows\temp\steam.exe");
             System.Diagnostics.Process.Start(@"C:\windows\temp\steam.exe");
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            Kaplan.DownloadFile("https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi", @"c:\windows\temp\EpicGamesLauncher.exe");
+            MessageBox.Show("Uygulama yükleniyor...");
+            Tiger.DownloadFile("https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi", @"c:\windows\temp\EpicGamesLauncher.exe");
             System.Diagnostics.Process.Start(@"C:\windows\temp\EpicGamesLauncher.exe");
         }
 
